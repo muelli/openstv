dee8ebaf9a35176edb2e3cfe403c1ba932451aa7
Rafik Nasraoui <rafik.nasraoui@gmail.com>
2017-03-02 22:23:31 +1300

    Code clean up

--- openstv/runRanking.py
+++ openstv/runRanking.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-# "run an election from the command line with optional profiling"
-
-# __revision__ = "$Id: runElection.py 715 2010-02-27 17:00:55Z jeff.oneill $"
+"run a ranking from the command line with optional profiling"
 
 import sys
 import os
@@ -9,7 +7,6 @@ import re
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import getopt
-import shutil
 
 from openstv.ballots import Ballots
 from openstv.plugins import getMethodPlugins, getReportPlugins
@@ -22,10 +19,25 @@ reports = getReportPlugins("byName", exclude0=False)
 reportNames = reports.keys()
 reportNames.sort()
 
+
+""" Util REGEX to read a BLT file line by line """
+blankLineRE = re.compile(r'^\s*(?:#.*)?$')
+nCandnSeatsRE = re.compile(r'^\s*(\d+)\s+(\d+)\s*(?:#.*)?$')
+withdrawnRE = re.compile(r'^\s*(-\d+(?:\s+-\d+)*)\s*(?:#.*)?$')
+ballotRE = re.compile(r'^\s*(\d+(?:\s+[\d\-=]+)*)\s+0\s*(?:#.*)?$')
+ballotAndIDRE = re.compile(r'^\s*\(([^\)]+)\)\s+(\d+(?:\s+[\d\-=]+)*)\s+0\s*(?:#.*)?$')
+endOfBallotsRE = re.compile(r'\s*0\s*(?:#.*)?')
+stringRE = re.compile(r'^\s*"([^"]+)"\s*(?:#.*)?$')
+
+
+""" tmp folder for intermediate ballot files """
+BALLOTS_DIR = 'tmp_ballots/'
+
+
 usage = """
 Usage:
 
-  runRanking.py [-p prec] [-r report] [-t tiebreak] [-w weaktie] 
+  runElection.py [-p prec] [-r report] [-t tiebreak] [-w weaktie] [-s seats] 
                  [-P] [-x reps] method ballotfile
 
   -p: override default precision (in digits)
@@ -36,15 +48,16 @@ Usage:
   -x: specify repeat count (for profiling)
     *default
 
-  Runs an election for the given method and ballot file. Results are
+  Runs a ranking for the given method and ballot file. Results are
   printed to stdout. The following methods are available:
 %s
 """ % (", ".join(reportNames),
        "\n".join(["    " + name for name in methodNames]))
 
+
 # Parse the command line.
 try:
-  (opts, args) = getopt.getopt(sys.argv[1:], "Pp:r:l:t:w:x:")
+  (opts, args) = getopt.getopt(sys.argv[1:], "Pp:r:t:w:x:")
 except getopt.GetoptError, err:
   print str(err) # will print something like "option -a not recognized"
   print usage
@@ -52,10 +65,10 @@ except getopt.GetoptError, err:
 
 profile = False
 reps = 1
-reportformat = "HtmlReport"
+reportformat = "TextReport"
 strongTieBreakMethod = None
 weakTieBreakMethod = None
-numSeats = None
+#numSeats = None
 prec = None
 for o, a in opts:
   if o == "-r":
@@ -67,8 +80,8 @@ for o, a in opts:
       sys.exit(1)
   if o == "-p":
     prec = int(a)
-  if o == "-s":
-    print "Seats option unsupported when ranking"
+  # if o == "-s":
+  #   numSeats = int(a)
   if o == "-t":
     if a in ["random", "alpha", "index"]:
       strongTieBreakMethod = a
@@ -108,76 +121,8 @@ if name not in methodNames:
   sys.exit(1)
 
 
-blankLineRE = re.compile(r'^\s*(?:#.*)?$')
-nCandnSeatsRE = re.compile(r'^\s*(\d+)\s+(\d+)\s*(?:#.*)?$')
-withdrawnRE = re.compile(r'^\s*(-\d+(?:\s+-\d+)*)\s*(?:#.*)?$')
-ballotRE = re.compile(r'^\s*(\d+(?:\s+[\d\-=]+)*)\s+0\s*(?:#.*)?$')
-ballotAndIDRE = re.compile(r'^\s*\(([^\)]+)\)\s+(\d+(?:\s+[\d\-=]+)*)\s+0\s*(?:#.*)?$')
-endOfBallotsRE = re.compile(r'\s*0\s*(?:#.*)?')
-stringRE = re.compile(r'^\s*"([^"]+)"\s*(?:#.*)?$')
-
-baseFileName = os.path.splitext(bltFn)[0]
-extension = os.path.splitext(bltFn)[1]
-
-BALLOTS_DIR = 'ballots/'
-
-def updateBallots(cleanBallots, excluded, rank):
-  if not os.path.exists('ballots'):
-    os.makedirs('ballots')
-
-  # Sanity check
-  if cleanBallots.numSeats != rank - 1: 
-    print "ERROR: something went wrong", cleanBallots.numSeats, rank
-
-  copy = cleanBallots.copy()
-  copy.numSeats = (cleanBallots.numSeats - 1)
-  copy.saveAs(BALLOTS_DIR + baseFileName + ".tmp")
-
-  newLines = []
-  candidateIndex = 0
-  for line in open(BALLOTS_DIR + baseFileName + ".tmp"):
-    out = nCandnSeatsRE.match(line)
-    if out is not None: 
-      newLines.append(str(cleanBallots.getNumCandidates() - 1) + " " + str(rank - 1) + "\n")
-      continue
-
-    out = ballotAndIDRE.match(line)
-    if out is not None:
-      ballotIndex = out.group(1)
-      rankings = out.group(2).split()[1:]
-      if str(excluded) in rankings:
-        rankings.remove(str(excluded))
-
-      newRankings = map(lambda c: c if (int(c) < excluded) else str(int(c) -1), rankings)
-
-      newLine = "(" + ballotIndex +") 1 "
-      for vote in newRankings:
-        newLine += str(vote) + " "
-      newLine += "0\n"
-      newLines.append(newLine)
-
-      continue
-
-    out = stringRE.match(line)
-    if out is not None:
-      if candidateIndex != -1: # excluded candidate not met yet
-        if candidateIndex == excluded:
-          candidateIndex = -1
-          continue # Skip this candidate since they have been excluded
-        else:
-          candidateIndex += 1
-
-    newLines.append(line)
-
-
-  os.remove(BALLOTS_DIR + baseFileName + ".tmp")
-  f  = open(BALLOTS_DIR + baseFileName + "." + str(rank - 1) + extension, 'w+')
-  f.writelines(newLines)
-  f.close()
-
-
-def doElection(reps=1):
-  "run election with repeat count for profiling"
+def doElection(cleanBallots, reps=1):
+  """ run election with repeat count for profiling """
   for i in xrange(reps):
     e = methods[name](cleanBallots)
     if strongTieBreakMethod is not None:
@@ -190,86 +135,137 @@ def doElection(reps=1):
   return e
 
 
-firstRound = True
-initialCandidates, candidates = ([],)*2
-finalRanking = {}
+def alterBallots(ballots, loser):
+  """ 
+    Alter ballots by removing votes for the loser candidate
 
-## This is a bit redundant
-dirtyBallots = Ballots()
-dirtyBallots.loadKnown(bltFn, exclude0=False)
-lastRank = dirtyBallots.getCleanBallots().getNumCandidates()
-initialCandidates = set(range(0, lastRank))
+    ballots: Current ballots object
+    loser: index of last eliminated candidate in current ballot
+  """
 
-for rank in range(lastRank, 0, -1):
-  try:
-    if not firstRound:
-      bltFn = 'ballots/'+baseFileName+ "." + str(rank) + extension
+  nextRank = ballots.getNumCandidates() - 1
+  seats = ballots.getNumCandidates() - 2
 
-    dirtyBallots = Ballots()
-    dirtyBallots.loadKnown(bltFn, exclude0=False)
-    dirtyBallots.numSeats = rank - 1
-    cleanBallots = dirtyBallots.getCleanBallots()
+  if not os.path.exists(BALLOTS_DIR):
+    os.makedirs(BALLOTS_DIR)
 
-    if firstRound: 
-      candidates = initialCandidates
-      firstRound = False
+  tmpFile = BALLOTS_DIR + "_tmp.blt"
 
-  except RuntimeError, msg:
-    print msg
-    sys.exit(1)
+  copy = ballots.copy()
+  copy.numSeats = (ballots.numSeats - 1)
+  copy.saveAs(tmpFile)
 
-  if profile:
-    cProfile.run('e = doElection(reps)', profilefile)
-  else:
-    e = doElection()
-  r = reports[reportformat](e)
-  r.generateReport()
-  if profile:
-    p = pstats.Stats(profilefile)
-    p.strip_dirs().sort_stats('time').print_stats(50)
+  newLines = []
+  candidateIndex = 0
+  for line in open(tmpFile):
+    out = nCandnSeatsRE.match(line)
 
-  eliminatedSet = candidates.difference(e.winners)
-  if len(eliminatedSet) > 1:
-    print "More than ONE candidate eliminated:", candidates.difference(e.winners)
-    sys.exit(1)
+    """ 
+      Append the first line of the new ballot file:
+      #Candidates #Seats 
+    """
+    if out is not None: 
+      newLines.append(str(nextRank) + " " + str(seats) + "\n")
+      continue
 
-  eliminated = eliminatedSet.pop()
-  eliminatedName = cleanBallots.getNames()[eliminated]
-  finalRanking[rank] = eliminatedName
 
+    """
+      Alter old ballots by removing votes for excluded candidate
+      (index) 1 # # # # # 0 \n
+    """
+    out = ballotAndIDRE.match(line)
+    if out is not None:
+      ballotIndex = out.group(1)
+      rankings = out.group(2).split()[1:]
+      if str(loser) in rankings:
+        rankings.remove(str(loser))
+
+      """ 
+        apply a lambda function to "bump" candidates that were ranked below the excluded candidate,
+        other candidates keep their original rank
+      """
+      newRankings = map(lambda c: c if (int(c) < loser) else str(int(c) -1), rankings)
+
+      newLine = "(" + ballotIndex +") 1 "
+      for vote in newRankings:
+        newLine += str(vote) + " "
+      newLine += "0\n"
+      newLines.append(newLine)
+
+      continue
+
+    """ 
+      Remove excluded candidate name
+    """
+    out = stringRE.match(line)
+    if out is not None:
+      if candidateIndex != -1: # excluded candidate not met yet
+        if candidateIndex == loser:
+          candidateIndex = -1
+          continue # Skip this candidate since they have been excluded
+        else:
+          candidateIndex += 1
+
+    """
+      Append the rest of candidate names to the new file
+    """
+    newLines.append(line)
+
+
+  """ 
+    write file
+  """
+  os.remove(tmpFile)
+  newFileName = BALLOTS_DIR + "_.%s.blt" % str(nextRank) 
+  f  = open(newFileName, 'w+')
+  f.writelines(newLines)
+  f.close()
   
-  roundLine = """\
-  --
-    Rank: %d
-    Eliminated: %s (index in file %s: %d)\
-  """ % (rank, eliminatedName, bltFn,eliminated)
-  print roundLine
+  dirtyBallots = Ballots()
+  dirtyBallots.loadKnown(newFileName, exclude0=False)
+  return dirtyBallots
 
-  candidates = set(map(lambda c: c if (c < eliminated) else c - 1, e.winners))
-  if len(candidates) == 1: # one candidate is left, they are ranked #1
-    # print "Winner - Rank 1\n"
-    winner = e.winners.pop()
-    winnerName = cleanBallots.getNames()[winner]
-    # roundLine = """\
-    # --
-    #   Rank: 1
-    #   Winner: %s (index in file %s: %d)\
-    # """ % (winnerName, bltFn, winner)
-    # print roundLine    
-    finalRanking[1] = winnerName
-    break;
-  else:
 
-    updateBallots(cleanBallots, eliminated, rank)
+# Create ballots
+try:
 
-print """<br>
-  <h3>Final Ranking</h3>
-  <table class="rounds">
-    <tr>
-      <th>Rank</th>
-      <th>Candidate</th>
-    </tr>
-"""
-for rank in finalRanking:
-  print "<tr><td>%d</td><td>%s</td></tr>" % (rank, finalRanking[rank])
-print "</table>"
+  dirtyBallots = Ballots()
+  dirtyBallots.loadKnown(bltFn, exclude0=False)
+  lastRank = dirtyBallots.getNumCandidates()
+  initialBallots = dirtyBallots.getCleanBallots()
+
+  for i in reversed(xrange(lastRank)):
+    names = initialBallots.getNames()
+    e = doElection(initialBallots)
+
+    r = reports[reportformat](e)
+    r.generateReport()
+
+    loser = e.losers.pop()
+    print i+1, names[loser]
+
+    if (len(e.winners) == 1):
+      print i, names[e.winners.pop()]
+      break
+
+    initialBallots = alterBallots(initialBallots, loser)
+  # print "Rank", lastRank, ":", initalBallots.getNames()[e.losers.pop()]
+
+except RuntimeError, msg:
+  print msg
+  sys.exit(1)
+
+
+
+
+# if profile:
+#   cProfile.run('e = doElection(reps)', profilefile)
+# else:
+#   e = doElection()
+
+# r = reports[reportformat](e)
+# r.generateReport()
+
+# if profile:
+#   p = pstats.Stats(profilefile)
+#   p.strip_dirs().sort_stats('time').print_stats(50)
